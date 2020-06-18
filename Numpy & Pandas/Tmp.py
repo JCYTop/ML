742fd490120ef9d1407a33629dd8cf8819916e8a
JCY_Com <jcyemail@qq.com>
2020-06-18 20:35:04 +0800

    更新

--- Numpy & Pandas/Tmp.py
+++ Numpy & Pandas/Tmp.py
@@ -25,6 +25,7 @@ print("---------------------------")
 a = np.array([[1, 1], [0, 1]])
 b = np.arange(4).reshape((2, 2))
 print(a)
+print(a.T)
 print(b)
 c = a * b
 c_dot = np.dot(a, b)
@@ -34,3 +35,28 @@ print(c_dot)
 
 a = np.random.random(2)
 print(np.min(a))
+
+print(" ")
+print("pandas-------------------------------------")
+import pandas as pd
+
+dates = pd.date_range("20200605", periods=6)
+df = pd.DataFrame(np.arange(24).reshape((6, 4)), index=dates, columns=['A', 'B', 'C', 'D'])
+print(df)
+# print(df['A'])
+# print(df.A)
+print(df.loc['20200608', 'A':])
+
+print(" ")
+print(" -------------------------------------")
+import matplotlib.pyplot as plt
+
+# Series
+data = pd.Series(np.random.randn(1000), index=np.arange(1000))
+data = data.cumsum()
+
+# DataFrame
+data = pd.DataFrame(np.random.randn(1000, 4), columns=list("ABsD"))
+
+data.plot()
+plt.show()
